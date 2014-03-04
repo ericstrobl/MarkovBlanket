9ba646c525ffa217d0e23fc66bdb18aaa61a314d
Eric V. Strobl <ericvonstrobl@gmail.com>
2014-03-04 00:38:16 -0500

    Update demo.m

--- demo.m
+++ demo.m
@@ -2,6 +2,10 @@
 
 [data] = CreateArtificialMB();
 
-[Ranked,KCDM] = BackCD(data,4);
+[Ranked,KCDM] = BackCD(data,4,'reg');
 
-[Ranked,KCDM] = ForCD(data,4,5);
+[Ranked,KCDM] = ForCD(data,4,5,'reg');
+
+[Ranked,KDM] = BAHSIC(data,4,'reg');
+
+[Ranked,KDM] = FOHSIC(data,4,5,'reg');
